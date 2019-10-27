5f87a49175fca2db145075fe094c781313cb077f
Oliver Chen <oliverc2@illinois.edu>
2019-10-27 01:58:35 -0500

    hello

--- client/src/App.js
+++ client/src/App.js
@@ -2,45 +2,20 @@ import React, { Component } from 'react'
 import './App.css'
 class App extends Component {
   state = {
-    cow: '',
     text: ''
   }
 componentDidMount() {
-    this.fetchCow()
+    this.fetchGreeting();
   }
-fetchCow = async () => {
-    const response = await fetch(`/api/cow`)
-    const initialCow = await response.json()
-    const cow = initialCow.moo
-    this.setState({ cow })
-  }
-customCow = async evt => {
-    evt.preventDefault()
-    const text = this.state.text
-    const response = await fetch(`/api/cow/${text}`)
-    const custom = await response.json()
-    const cow = custom.moo
-    this.setState({ cow, text: '' })
-  }
-handleChange = evt => {
-    this.setState({ [evt.target.name]: evt.target.value })
-    console.log(this.state.text)
+fetchGreeting = async () => {
+    const response = await fetch('/api/greet');
+    const resJSON= await response.json();
+    this.setState({text:resJSON.text});
   }
 render() {
     return (
       <div className="App">
-        <h3>Text Cow. Moo</h3>
-        <code>{this.state.cow}</code>
-        <form onSubmit={this.customCow}>
-          <label>Custom Cow Text:</label>
-          <input
-            type="text"
-            name="text"
-            value={this.state.text}
-            onChange={this.handleChange}
-          />
-          <button type="submit">Show me a talking cow!</button>
-        </form>
+        <h3>{this.state.text}</h3>
       </div>
     )
   }
--- index.js
+++ index.js
@@ -3,21 +3,11 @@ const cowsay = require('cowsay')
 const cors = require('cors')
 // Create the server
 const app = express()
-// Serve our api route /cow that returns a custom talking text cow
-app.get('/api/cow/:say', cors(), async (req, res, next) => {
+// Serve our base route that returns a Hello World greeting
+app.get('/api/greet/', cors(), async (req, res, next) => {
   try {
-    const text = req.params.say
-    const moo = cowsay.say({ text })
-    res.json({ moo })
-  } catch (err) {
-    next(err)
-  }
-})
-// Serve our base route that returns a Hello World cow
-app.get('/api/cow/', cors(), async (req, res, next) => {
-  try {
-    const moo = cowsay.say({ text: 'Hello World!' })
-    res.json({ moo })
+    const greeting={ text: 'Hello World!' };
+    res.json(greeting)
   } catch (err) {
     next(err)
   }
