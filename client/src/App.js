c16df4e5d382601826ab3e410eba802c8a6f0699
Oliver Chen <oliverc2@illinois.edu>
2019-10-28 00:55:01 -0500

    add insert form

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import './App.css'
 class App extends Component {
   state = {
     text: '',
-    first: ''
+    random: ''
   }
 componentDidMount() {
     this.fetchGreeting();
@@ -13,17 +13,60 @@ fetchGreeting = async () => {
     const resJSON= await response.json();
     this.setState({text:resJSON.text});
   }
-fetchFirst = async()=>{
+fetchRandom = async()=>{
   const response = await fetch('/api/getFirst');
   const resJSON= await response.json();
-  this.setState({first:"\n Type "+resJSON.type+ "\n Name: "+resJSON.name});
+  this.setState({random:"\n Type "+resJSON.type+ "\n Name: "+resJSON.name});
 }
+handleSubmit(event) {
+    event.preventDefault();
+    const insertInputs =event.target.getElementsByClassName("insert");
+    console.log("name:"+insertInputs.name.value);
+    console.log("type:"+insertInputs.type.value);
+    console.log("gender:"+insertInputs.gender.value);
+    console.log("price:"+insertInputs.price.value);
+    console.log("WebsiteURL:"+insertInputs.websiteURL.value);
+    console.log("BrandName:"+insertInputs.BrandName.value);
+    document.getElementById("insertForm").reset();
+  }
 render() {
     return (
       <div className="App">
         <h3>{this.state.text}</h3>
-        <button type='submit' onClick={this.fetchFirst}>Show First</button>
-        <h3>{this.state.first}</h3>
+        <button type='submit' onClick={this.fetchRandom}>Show Random</button>
+        <h3>{this.state.random}</h3>
+        <form id="insertForm" onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input className="insert" type="text" name="name" required/>
+          </label>
+          <label>
+            Type:
+            <input className="insert" type="text" name="type" required/>
+          </label>
+          <label>
+          Gender:
+          <select name="gender" className="insert" defaultValue="Male" onChange={this.handleChange}>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+          </select>
+        </label>
+        <label>
+          Price:
+          <input className="insert" type="number" step="0.01" name="price" required/>
+        </label>
+        <label>
+          Website URL:
+          <input className="insert" type="url" name="websiteURL" placeholder="https://example.com"
+                  pattern="https://.*"
+                  required/>
+        </label>
+        <label>
+          Brand Name:
+          <input className="insert" type="text" name="BrandName" required/>
+        </label>
+          <input type="submit" value="Insert Clothing" />
+        </form>
       </div>
     )
   }
