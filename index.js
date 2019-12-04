19db24c8d9f5bb7b351d5cb3510d7a0de3286d67
Allen <allenc3@illinois.edu>
2019-12-04 16:56:31 -0600

    res.send?

--- index.js
+++ index.js
@@ -377,7 +377,7 @@ app.get('/api/knn/:season', cors(), async (req, res, next) => {
   var spawn = require("child_process").spawn;
   var process = spawn('python3', ["nn/cnn_code/knn.py", req.params.season]);
   //
-  process.stdout.on('data', (data) => {
+  process.stdout.on('data', async function(data){
     let data_string = await data.toString();
     res.json(data_string);
   });
